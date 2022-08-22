4e41971c3336de9fc25a94a654fc1eb540b270a8
NikitaBarinov <niskill201@yandex.ru>
2022-08-22 15:07:30 +0700

    createProfile

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,5 @@
 import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { AddProfileDto } from 'src/users/dto/add-profile.dto';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { ProfileService } from './profile.service';
 
@@ -7,12 +8,7 @@ export class ProfileController {
     constructor(private profileService: ProfileService) {}
 
     @Post()
-    create(@Body() dto: CreateProfileDto) {
+    create(@Body() dto: AddProfileDto) {
         return this.profileService.createProfile(dto);
     }
-
-    // @Get('/:username')
-    // getByUsername(@Param('username') username: string) {
-    //     return this.profileService.getProfileByValue(username);
-    // }
 }
